3a8f94f5f5af2c6bf5ec3c4cfbed221442a46114
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:39:29 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "cosmosdb_mongo_collections" {
+  description = "All cosmosdb_mongo_collection resources"
+  value       = azurerm_cosmosdb_mongo_collection.cosmosdb_mongo_collections
+}
+
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "cosmosdb_mongo_collections" {
+  description = <<EOT
+Map of cosmosdb_mongo_collections, attributes below
+Required:
+    - account_name
+    - database_name
+    - name
+    - resource_group_name
+Optional:
+    - analytical_storage_ttl
+    - default_ttl_seconds
+    - shard_key
+    - throughput
+    - autoscale_settings (block):
+        - max_throughput (optional)
+    - index (block):
+        - keys (required)
+        - unique (optional)
+EOT
+
+  type = map(object({
+    account_name           = string
+    database_name          = string
+    name                   = string
+    resource_group_name    = string
+    analytical_storage_ttl = optional(number)
+    default_ttl_seconds    = optional(number)
+    shard_key              = optional(string)
+    throughput             = optional(number)
+    autoscale_settings = optional(object({
+      max_throughput = optional(number)
+    }))
+    index = optional(object({
+      keys   = list(string)
+      unique = optional(bool, false)
+    }))
+  }))
+}
+
